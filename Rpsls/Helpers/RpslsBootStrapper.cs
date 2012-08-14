4e255575a2a33154b1efa661b91aef1298d3752a
Juan J. Chiw <jjchiw@gmail.com>
2012-08-14 20:51:34 +0200

    damn csproj

--- Rpsls/Helpers/RpslsBootStrapper.cs
+++ Rpsls/Helpers/RpslsBootStrapper.cs
@@ -50,7 +50,7 @@ namespace Rpsls.Helpers
 			// As this is now per-request we could inject a request scoped
 			// database "context" or other request scoped services.
 
-			var parser = ConnectionStringParser<RavenConnectionStringOptions>.FromConnectionStringName("RAVENHQ_CONNECTION_STRING");
+			var parser = ConnectionStringParser<RavenConnectionStringOptions>.FromConnectionStringName("RavenDB");
 			parser.Parse();
 
 			var documentStore = new DocumentStore
